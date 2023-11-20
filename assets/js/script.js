70ebb5ba9177aff10584785f07a10f49f08d1733
Marius Du Plooy <woestyn101@gmail.com>
2023-11-20 11:27:20 -0500

    styled with css

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,14 @@ var randNumber = Math.floor(Math.random() * 5) + 1;
           //alert("your are correct");
           theAnswer.innerHTML =
             " Your are correct, The number was: " + randNumber;
+            theAnswer.style.color = "yellow";
         } else if (theInput > randNumber) {
           //alert("your are wrong!, the number was: " + randNumber);
           theAnswer.innerHTML = "Guess lower";
+          theAnswer.style.color = "red";
         } else {
           theAnswer.innerHTML = " Guess higher";
+          theAnswer.style.color = "green";
         }
 
         // alert("hello");
